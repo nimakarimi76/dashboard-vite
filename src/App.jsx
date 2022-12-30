ead4f4e06475a7ea76a03e78c2c10ba23ccebe19
nimakarimi76 <nimakarimi676@gmail.com>
2022-12-31 00:00:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
